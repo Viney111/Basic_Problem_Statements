ed9bdcd08c071d5ac1df684d20d8db89f0344bec
Viney <vineykhaneja999@gmail.com>
2022-02-11 16:58:17 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic
+{
+    internal class ProblemStatements
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the solutions of basic MATHEMATICAL PROBLEM STATEMENTS");
+        }
+
+    }
+}
